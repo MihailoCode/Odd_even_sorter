28a6ba91d41fdb5e8e6353e63314a18d926a1cbd
MihailoCode <43187979+MihailoCode@users.noreply.github.com>
2023-10-29 15:35:07 +0100

    Update top_sorter.v

--- src/top_sorter.v
+++ src/top_sorter.v
@@ -4,9 +4,13 @@ module top_sorter(clk,rst_n,inp,out);
     input [7:0] inp;
     output [7:0] out;
 
-    assign inp[7:4]=4'b0011;
-    assign inp[3:0]=4'b0010;
-    assign out=inp[4:4]*inp[3:0];
+    wire [3:0] a;
+    wire [3:0] b;
+
+    assign a=4'b0011;
+    assign b=4'b0010;
+    
+    assign out=a*b;
 
    
 
