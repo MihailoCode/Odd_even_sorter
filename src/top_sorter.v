150f20b0cc51635b0f1dc015e0364b63aef53e48
MihailoCode <43187979+MihailoCode@users.noreply.github.com>
2023-10-30 08:41:30 +0100

    Update top_sorter.v

--- src/top_sorter.v
+++ src/top_sorter.v
@@ -1,17 +1,16 @@
-module top_sorter(clk,rst_n,inp,out);
-    input clk;
-    input rst_n;
-    input [7:0] inp;
-    output [7:0] out;
+module top_sorter(
+    input [7:0] inp,
+    input [7:0] out
+);
 
-    wire [3:0] a;
-    wire [3:0] b;
-
-    assign a=4'b0011;
-    assign b=4'b0010;
-    
-    assign out=a*b;
-
+    perceptron p11(inp,8'd2,8'd4,8'd2,8'd1,8'd5,8'd2,8'd2,8'd2,out[0]);
+    perceptron p12(inp,8'd1,8'd2,8'd3,8'd4,8'd2,8'd2,8'd2,8'd1,out[1]);
+    perceptron p13(inp,8'd1,8'd2,8'd4,8'd2,8'd1,8'd5,8'd2,8'd2,out[2]);
+    perceptron p14(inp,8'd1,8'd4,8'd2,8'd5,8'd2,8'd3,8'd3,8'd1,out[3]);
+    perceptron p15(inp,8'd1,8'd2,8'd2,8'd3,8'd1,8'd2,8'd2,8'd2,out[4]);
+    perceptron p16(inp,8'd3,8'd3,8'd3,8'd3,8'd2,8'd2,8'd2,8'd2,out[5]);
+    perceptron p17(inp,8'd4,8'd3,8'd2,8'd1,8'd2,8'd2,8'd3,8'd3,out[6]);
+    perceptron p18(inp,8'd4,8'd4,8'd1,8'd1,8'd2,8'd2,8'd3,8'd4,out[7]);
    
 
 endmodule
