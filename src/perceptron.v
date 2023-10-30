c982b4aaa8d09e32ee38f6242614093ffe2d3e65
MihailoCode <43187979+MihailoCode@users.noreply.github.com>
2023-10-30 09:34:54 +0100

    Update perceptron.v

--- src/perceptron.v
+++ src/perceptron.v
@@ -8,10 +8,11 @@ input [7:0] weight4,
 input [7:0] weight5,
 input [7:0] weight6,
 input [7:0] weight7,
+input [7:0] bias,
 output [7:0] out
 );
 
-assign out=in[0]*weight0+in[1]*weight1+in[2]*weight2+in[3]*weight3+in[4]*weight4+in[5]*weight5+in[6]*weight6+in[7]*weight7;
+assign out=in[0]*weight0+in[1]*weight1+in[2]*weight2+in[3]*weight3+in[4]*weight4+in[5]*weight5+in[6]*weight6+in[7]*weight7+bias;
 
 
 
